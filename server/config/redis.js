29186b29556c64cad57a59ed05128132e1ed8223
riannurulh <riannurul57@gmail.com>
2024-08-21 19:26:34 +0700

    checkpoint

--- server/config/redis.js
+++ server/config/redis.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 const redis = new Redis({
     port: 14503, // Redis port
-    host: "redis-14503.c295.ap-southeast-1-1.ec2.redns.redis-cloud.com", // Redis host
+    host: process.env.REDIS_URL, // Redis host
     username: "default", // needs Redis >= 6
     password: "CRikzpDkrFgrqbUZkqO6mn0jHmG1iCYx",
     db: 0, // Defaults to 0
